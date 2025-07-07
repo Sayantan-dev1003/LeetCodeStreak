830edc5258ed5bd2520bc078c3f2924d348fe322
Sayantan Halder <144941687+Sayantan-dev1003@users.noreply.github.com>
2025-07-07 13:58:43 +0530

    Create Day146_MaxNumOfEventsAttended.cpp

---
+++ Day146_MaxNumOfEventsAttended.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(), events.end());
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        int day = 0, i = 0, res = 0, n = events.size();
+
+        while (i < n || !minHeap.empty()) {
+            if (minHeap.empty())
+                day = events[i][0];
+
+            while (i < n && events[i][0] <= day) {
+                minHeap.push(events[i][1]);
+                i++;
+            }
+
+            while (!minHeap.empty() && minHeap.top() < day) {
+                minHeap.pop();
+            }
+
+            if (!minHeap.empty()) {
+                minHeap.pop();
+                res++;
+                day++;
+            }
+        }
+
+        return res;
+    }
+};
