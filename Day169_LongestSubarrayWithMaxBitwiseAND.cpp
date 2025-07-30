a57d8fb6dad239c89d543311b500bd9ab2ed7c56
Sayantan Halder <144941687+Sayantan-dev1003@users.noreply.github.com>
2025-07-30 21:32:47 +0530

    Create Day169_LongestSubarrayWithMaxBitwiseAND.cpp

---
+++ Day169_LongestSubarrayWithMaxBitwiseAND.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        int longest = 0, currentLength = 0;
+
+        for (int num : nums) {
+            if (num == maxVal) {
+                currentLength++;
+                longest = max(longest, currentLength);
+            } else {
+                currentLength = 0;
+            }
+        }
+        return longest;
+    }
+};
